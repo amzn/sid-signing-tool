c733a65599344d8880e7e0386f001c06431bbfb7
Carlos Yeh <shyeh@amazon.com>
2024-06-16 21:55:00 +0800

    Move flat layout to src layout

--- src/sid_signing_tool/__main__.py
+++ src/sid_signing_tool/__main__.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import sys
-from cli import main as _main
+from sid_signing_tool.cli import main as _main
 
 if __name__ == "__main__":
     sys.exit(_main())
